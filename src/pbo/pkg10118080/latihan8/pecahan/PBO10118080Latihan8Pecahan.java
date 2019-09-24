7eb143014b04fb33588718ed064363ca3381cd3e
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:34:31 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan8/pecahan/PBO10118080Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan jenis-jenis tipe data pecahan
+ */
+public class PBO10118080Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        float f;
+        double d;
+        f=1234567890.123456789F;
+        d=1_234_567_890.123456789;
+        System.out.println("Float : "+f);
+        System.out.println("Double : "+d);
+    }
+    
+}
